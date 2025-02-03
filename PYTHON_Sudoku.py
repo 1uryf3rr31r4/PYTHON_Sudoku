21c9e5704063cd769daa61f87aeb2b84984b5789
1uryf3rr31r4 <1uryf3rr31r4@gmail.com>
2025-02-03 09:59:04 -0300

    Tela do Jogo, Inicializando fonte e Escolha de tamanho e fonte

--- PYTHON_Sudoku.py
+++ PYTHON_Sudoku.py
@@ -1,3 +1,16 @@
 import pygame as pg
 import random
 import math
+
+preto = (0,0,0)
+vermelho = (255,0,0)
+verde = (0,255,0)
+azul_claro = (200,200,255)
+azul = (100,100,255)
+branco = (255,255,255)
+
+window = pg.display.set_mode((1000,700))
+
+pg.font.init()
+
+fonte = pg.font.SysFont("Courier New", 50, bold=True)
